bd7b2a6720a028d4727ff7acfb37ba91a554eb27
OindrilaDas27 <oindrila.das2708@gmail.com>
2024-11-06 22:59:56 +0530

    add gitignore

--- utils/encodePassword.js
+++
@@ -1,9 +0,0 @@
-var bcrypt = require('bcrypt');
-
-exports.encryptPassword = async (password) => {
-    return await bcrypt.hash(password, 10)
-};
-
-exports.comparePassword = async (plainPass, hashword) => {
-    return await bcrypt.compare(plainPass, hashword)
-};
